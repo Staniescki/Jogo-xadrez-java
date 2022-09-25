940845e43e5ee1143bab99a3f22df1a23df228a6
Diego Staniescki <Diego.stk@live.com>
2022-09-25 13:14:53 -0300

    First Class position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World");
+		
+		Position po = new Position(10, 20);
+		System.out.println(po);
 
 	}
 
